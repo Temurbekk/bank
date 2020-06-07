125b6605fabf97f0329de488ac4c81c01c9eb6fa
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 15:31:06 -0400

    Implement credit page

--- src/Pages/Credit.jsx
+++ src/Pages/Credit.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Credit(props) {
-  return <div>{props.creditTotal}</div>;
+  return (
+    <div>
+      <div>This is the Credit page</div>
+      <div>Current Account Balance {props.accountBalance}</div>
+      <div> Credit Total:{props.creditTotal}</div>
+      <div>Add Credit spending</div>
+      <input type="text" placeholder="Enter a description..." />
+      <input type="text" placeholder="Enter dollar amount" />
+      <div>Add</div>
+      <div>
+        {props.credits.map((credit) => {
+          const date = new Date(credit.date);
+          return (
+            <div>
+              <div>Description: {credit.description}</div>
+              <div>Price: {credit.amount}</div>
+              <div>Date: {date.toLocaleDateString()}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Credit;
