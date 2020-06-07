ce615fe05c0efaeec17dbccd35b75d3d6c339c0c
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 12:09:42 -0400

    Set fetch credit

---
+++ src/FetchData/useFetchCredit.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function useFetchCredit() {
+  const [credits, setCredits] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [creditTotal, setCreditTotal] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://moj-api.herokuapp.com/credits")
+      .then((response) => {
+        let data = response.data;
+        setCredits(data);
+
+        let creditTotal = 0;
+        for (let obj of data) {
+          creditTotal += obj.amount;
+        }
+        setCreditTotal(creditTotal);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return { credits, isLoading, creditTotal };
+}
+
+export default useFetchCredit;
