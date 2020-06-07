c3188f29f840a6b541c0be3fb9e973c4958af97f
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 16:56:27 -0400

    Style login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,16 +6,50 @@ const Login = ({ setSignedIn }) => {
   };
   return (
     <>
-      <div>Welcome to React Bank</div>
-      <form action="">
-        <label>Username</label>
-        <input type="text" placeholder="username" />
-        <label>Password</label>
-        <input type="text" placeholder="Password" />
-        <div className="button" onClick={onClick}>
-          Log In
+      <section className="hero is-primary is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                <form action="" className="box">
+                  <div className="field">
+                    <label for="" className="label">
+                      Email
+                    </label>
+                    <div className="control">
+                      <input
+                        type="email"
+                        placeholder="e.g. bobsmith@gmail.com"
+                        className="input"
+                      />
+                      <span className="icon is-small is-left">
+                        <i className="fa fa-envelope"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label for="" className="label">
+                      Password
+                    </label>
+                    <div className="control ">
+                      <input
+                        type="password"
+                        placeholder="*******"
+                        className="input"
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <button className="button is-success" onClick={onClick}>
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
+      </section>
     </>
   );
 };
