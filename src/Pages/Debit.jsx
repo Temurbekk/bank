870c243fb2960fb63e6904666c85f8e7bf12ea51
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 15:30:06 -0400

    Implement Debit page

--- src/Pages/Debit.jsx
+++ src/Pages/Debit.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Debit(props) {
-  return <div> Debit Total:{props.debitTotal}</div>;
+  return (
+    <div>
+      <div>This is the debit page</div>
+      <div>Current Account Balance {props.accountBalance}</div>
+      <div> Debit Total:{props.debitTotal}</div>
+      <div>Add Debit spending</div>
+      <input type="text" placeholder="Enter a description..." />
+      <input type="text" placeholder="Enter dollar amount" />
+      <div>Add</div>
+      <div>
+        {props.debits.map((debit) => {
+          const date = new Date(debit.date);
+          return (
+            <div>
+              <div>Description: {debit.description}</div>
+              <div>Price: {debit.amount}</div>
+              <div>Date: {date.toLocaleDateString()}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Debit;
