236a33f1392a5f263f65727fa884b166a266916e
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 13:28:49 -0400

    Move navbar out of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -10,23 +10,61 @@ import useFetchCredit from "./FetchData/useFetchCredit";
 function App() {
   const [signedIn, setSignedIn] = useState(false);
   const [accountBalance, setAccountBalance] = useState(0);
-  const { credits, creditTotal } = useFetchCredit();
-  const { debits, debitTotal } = useFetchDebit();
   const [user, setUser] = useState({
     name: "Temur",
     account_num: 1234,
   });
+  const [credits, setCredits] = useState([]);
+  const [creditTotal, setCreditTotal] = useState(0);
+  const [debits, setDebits] = useState([]);
+  const [debitTotal, setDebitTotal] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://moj-api.herokuapp.com/credits")
+      .then((response) => {
+        let data = response.data;
+        setCredits(data);
+
+        let creditTotal = 0;
+        for (let obj of data) {
+          creditTotal += obj.amount;
+        }
+        setCreditTotal(creditTotal);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://moj-api.herokuapp.com/debits")
+      .then((response) => {
+        let data = response.data;
+        setDebits(data);
+
+        let debitTotal = 0;
+        for (let obj of data) {
+          debitTotal += obj.amount;
+        }
+        setDebitTotal(debitTotal);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   useEffect(() => {
     setAccountBalance(credits - debits);
     console.log(accountBalance);
-  }, [accountBalance, credits, debits]);
+  }, [credits, debits]);
 
   return (
     <>
+      <NavBar />
       {signedIn ? (
         <Router>
-          <NavBar />
           <Routes setSignedIn={setSignedIn} />
         </Router>
       ) : (
