0dda5bb2bdf52dd625c5066116b9467fd8bdb741
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 13:27:45 -0400

    Move fetching to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import NavBar from "./Components/NavBar";
 import Routes from "./Components/Routes";
 import Login from "./Pages/Login";
+import useFetchDebit from "./FetchData/useFetchDebit";
+import useFetchCredit from "./FetchData/useFetchCredit";
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
+  const [accountBalance, setAccountBalance] = useState(0);
+  const { credits, creditTotal } = useFetchCredit();
+  const { debits, debitTotal } = useFetchDebit();
+  const [user, setUser] = useState({
+    name: "Temur",
+    account_num: 1234,
+  });
+
+  useEffect(() => {
+    setAccountBalance(credits - debits);
+    console.log(accountBalance);
+  }, [accountBalance, credits, debits]);
+
   return (
     <>
       {signedIn ? (
