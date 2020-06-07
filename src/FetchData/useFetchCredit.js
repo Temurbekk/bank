e366a986efbf0d1074c4600604826e2f50410657
Temurbek Sabirov <prodota150@gmail.com>
2020-06-07 12:10:21 -0400

    Fix credit fetching

--- src/FetchData/useFetchCredit.js
+++ src/FetchData/useFetchCredit.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function useFetchCredit() {
   const [credits, setCredits] = useState([]);
